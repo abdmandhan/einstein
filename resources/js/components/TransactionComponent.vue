fa48ece08e3e36b56939b8f94516a8f5a7bb49a5
Abdurrahman Ramadhan <abdmandhan@gmail.com>
2020-11-15 16:16:24 +0000

    bukti transfer

--- resources/js/components/TransactionComponent.vue
+++ resources/js/components/TransactionComponent.vue
@@ -66,13 +66,45 @@
       </v-chip>
     </template>
     <template v-slot:[`item.image`]="{ item }">
-      <v-img max-height="150" max-width="250" :src="photo(item.image)"></v-img>
+      <v-dialog v-model="dialogPreview[item.id]">
+        <template v-slot:activator="{ on, attrs }">
+          <v-img
+            max-height="150"
+            max-width="250"
+            dark
+            @click.stop="$set(dialogPreview, item.id, true)"
+            :src="photo(item.image)"
+          >
+          </v-img>
+        </template>
+
+        <v-card>
+          <v-card-title class="headline grey lighten-2">
+            Bukti Transfer
+          </v-card-title>
+
+          <v-card-text>
+            <v-img dark :src="photo(item.image)"></v-img>
+          </v-card-text>
+
+          <v-divider></v-divider>
+
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn
+              color="primary"
+              text
+              @click.stop="$set(dialogPreview, item.id, false)"
+            >
+              Close
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
     </template>
     <template v-slot:[`item.actions`]="{ item }">
-      <span v-if="item.transaction_status_id != 2">
-        <v-btn color="primary" small> Accept </v-btn>
-        <v-btn color="danger" small> Reject </v-btn>
-      </span>
+      <v-btn color="primary" small> Accept </v-btn>
+      <v-btn color="danger" small> Reject </v-btn>
       <!-- <v-icon small class="mr-2" @click="editItem(item)"> mdi-pencil </v-icon>
       <v-icon small @click="deleteItem(item)"> mdi-delete </v-icon> -->
     </template>
@@ -112,6 +144,7 @@ export default {
     defaultItem: {
       id: "",
     },
+    dialogPreview: {},
   }),
 
   watch: {
