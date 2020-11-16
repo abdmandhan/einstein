f0b4196298d49ac898173c00a97d28187be43b71
Abdurrahman Ramadhan <abdmandhan@gmail.com>
2020-11-16 02:54:42 +0000

    login view handle

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('auth.login');
+    if (Auth::guest()) return view('auth.login');
+    else return redirect(route('home'));
 });
 
 Auth::routes();
