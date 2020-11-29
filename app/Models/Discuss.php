c8935ae9feb338d75b0d8d2779ebcf2eaa14edd0
Abdurrahman Ramadhan <abdmandhan@gmail.com>
2020-11-29 05:31:22 +0000

    discuss

--- app/Models/Discuss.php
+++ app/Models/Discuss.php
@@ -4,6 +4,12 @@ namespace App\Models;
 
 class Discuss extends BaseModel
 {
+
+    protected $casts = [
+        'created_at'    => 'date:Y-m-d H:i:s',
+        'is_done'       => 'boolean'
+    ];
+
     public function replies()
     {
         return $this->hasMany(DiscussReply::class, 'discuss_id');
