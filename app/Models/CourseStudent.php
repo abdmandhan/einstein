d71a31a78b8d3fe56bfb1772a596f932d5e5ddeb
Abdurrahman Ramadhan <abdmandhan@gmail.com>
2020-12-09 11:01:20 +0000

    student course

--- app/Models/CourseStudent.php
+++ app/Models/CourseStudent.php
@@ -6,6 +6,10 @@ namespace App\Models;
 
 class CourseStudent extends BaseModel
 {
+    protected $casts = [
+        'is_complete'    => 'boolean'
+    ];
+
     public function course()
     {
         return $this->belongsTo(Course::class, 'course_id');
